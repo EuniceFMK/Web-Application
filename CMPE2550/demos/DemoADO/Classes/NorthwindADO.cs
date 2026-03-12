0398d53a89b6dec22dcabfa19fbfb9a7806242ec
EuniceFMK <eunicengadjou2204@gmail.com>
2026-03-12 12:18:21 -0600

    Demo aco commit

---
+++ CMPE2550/demos/DemoADO/Classes/NorthwindADO.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DemoADO.Classes
+{
+    public class NorthwindADO
+    {
+        static string connection = "Server=data.cnt.sast.ca,24680;" +
+                                   "Database=efmukamngadjou1_Northwind;" +
+                                   "User Id=efmukamngadjou1;" +
+                                   "Password=Rachel1980@,.;" +
+                                   "Encrypt=False";
+
+        public static  List<List<string>> GetEmployees()
+        {
+            List<string> columnHeaders = new List<string>();
+            List<List<string>> rowData = new List<List<string>>();
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.ConnectionString = connection;
+                conn.Open();
+
+                string query = "SELECT * FROM Employees Where FirstName like 'A%'";
+                using (SqlCommand comm = new SqlCommand(query,conn))
+                {
+                    using (SqlDataReader reader = comm.ExecuteReader())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            columnHeaders.Add(reader.GetName(i));
+                        rowData.Add(columnHeaders);
+                        while (reader.Read())
+                        {
+                            List<string> row = new List<string>();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                                row.Add(reader[i]?.ToString());
+                            rowData.Add(row);
+                        }
+                       
+                    }
+                }
+            }
+            return rowData;
+        }
+
+        public static List<string> GetProductInfo(int productID)
+        {
+            List<string> productInfo = new List<string>();
+            
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+
+                conn.Open();
+
+                using (SqlCommand comm = new SqlCommand())
+                {
+                    comm.Connection = conn;
+                    comm.CommandType = CommandType.StoredProcedure;
+                    comm.CommandText = "GetProduct";
+
+                    SqlParameter pID = new SqlParameter();
+                    pID.ParameterName = "@productID";
+                    pID.Direction = ParameterDirection.Input;
+                    pID.SqlDbType = SqlDbType.Int;
+                    pID.Value = productID;
+
+                    comm.Parameters.Add(pID);
+                    using (SqlDataReader reader = comm.ExecuteReader())
+                    {
+                       
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                                productInfo.Add(reader[i]?.ToString());
+                        }
+
+                    }
+                }
+            }
+            return productInfo;
+        }
+
+
+    }
+}
